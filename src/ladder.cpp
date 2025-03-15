39abb1d88c030151d8b3ffbe70b80d88ebdbae9e
davebhakti <bhaktidave2206@gmail.com>
2025-03-15 19:40:36 +0000

    Added Dijkstras

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,9 +1,15 @@
 #include "ladder.h"
+#include <cassert>
 
 void error(string word1, string word2, string msg) {
-    cout << "Error: " << msg << endl;
+    if (word1.empty() && word2.empty()) {
+        cout << "Error: " << msg << endl;
+    } else {
+        cout << "Error (" << word1 << " -> " << word2 << "): " << msg << endl;
+    }
     exit(1);
 }
+
 
 bool edit_distance_within(const string& str1, const string& str2, int d) {
     int len1 = str1.length();
@@ -92,21 +98,12 @@ void print_word_ladder(const vector<string>& ladder) {
 }
 
 void verify_word_ladder() {
-
     set<string> word_list;
-
     load_words(word_list, "words.txt");
-
-    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
-
-    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
-
-    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
-
-    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
-
-    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
-
-    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
-
+    assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
