019a0892cb00eec9b26b789000165678f0ca1c0b
davebhakti <bhaktidave2206@gmail.com>
2025-03-15 09:14:31 +0000

    Added Dijkstras

---
+++ src/ladder.cpp
@@ -0,0 +1,112 @@
+#include "ladder.h"
+
+void error(string word1, string word2, string msg) {
+    cout << "Error: " << msg << endl;
+    exit(1);
+}
+
+bool edit_distance_within(const string& str1, const string& str2, int d) {
+    int len1 = str1.length();
+    int len2 = str2.length();
+    if (abs(len1 - len2) > d) {
+        return false;
+    }
+    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+
+    for (int i = 0; i <= len1; i++) {
+        for (int j = 0; j <= len2; j++) {
+            if (i == 0) {
+                dp[i][j] = j;
+            } else if (j == 0) {
+                dp[i][j] = i;
+            } else if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+            }
+        }
+    }
+
+    return dp[len1][len2] <= d;
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+
+    set<string> visited;
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+
+        string last_word = ladder.back();
+
+        for (const string& word : word_list) {
+            if (is_adjacent(last_word, word) && visited.find(word) == visited.end()) {
+                visited.insert(word);
+
+                vector<string> new_ladder = ladder;
+                new_ladder.push_back(word);
+
+                if (word == end_word) {
+                    return new_ladder;
+                }
+
+                ladder_queue.push(new_ladder);
+            }
+        }
+    }
+    return {};
+}
+
+void load_words(set<string>& word_list, const string& file_name) {
+    ifstream file(file_name);
+    if (!file) {
+        error("", "", "Unable to open dictionary file.");
+    }
+
+    string word;
+    while (file >> word) {
+        // Convert word to lowercase
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        word_list.insert(word);
+    }
+}
+
+void print_word_ladder(const vector<string>& ladder) {
+    if (ladder.empty()) {
+        cout << "No word ladder found." << endl;
+    } else {
+        cout << "Word ladder:" << endl;
+        for (const string& word : ladder) {
+            cout << word << " ";
+        }
+        cout << endl;
+    }
+}
+
+void verify_word_ladder() {
+
+    set<string> word_list;
+
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+
+}
