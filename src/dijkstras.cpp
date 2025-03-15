3db261a21c954114ca988cf0b5461e6313335769
davebhakti <bhaktidave2206@gmail.com>
2025-03-15 06:19:35 +0000

    Added Dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -45,18 +45,14 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
-        cout << "No path exists!" << endl;
+        // cout << "No path exists!" << endl;
         return;
     }
-    
-    cout << "Shortest path: ";
+
     for (size_t i = 0; i < path.size(); ++i) {
-        cout << path[i];
-        if (i < path.size() - 1) {
-            cout << " -> ";
-        }
+        cout << path[i] << " ";
     }
     cout << endl;
-    cout << "Total distance: " << total << endl;
+    cout << "Total cost is " << total << endl;
 }
 
